c7aa148ed80ce3ba3707dd35a008a793ccd8cb7f
5huvoo <shuvoszaman@gmail.com>
2018-02-18 00:25:45 +0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace alarm_clock_ver1._00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtp = new System.Windows.Forms.DateTimePicker();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtp
+            // 
+            this.dtp.CalendarMonthBackground = System.Drawing.SystemColors.InactiveCaption;
+            this.dtp.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtp.Location = new System.Drawing.Point(28, 55);
+            this.dtp.Name = "dtp";
+            this.dtp.Size = new System.Drawing.Size(210, 45);
+            this.dtp.TabIndex = 0;
+            // 
+            // b1
+            // 
+            this.b1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(12, 166);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(117, 50);
+            this.b1.TabIndex = 1;
+            this.b1.Text = "Start";
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // b2
+            // 
+            this.b2.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(146, 167);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(109, 49);
+            this.b2.TabIndex = 2;
+            this.b2.Text = "Stop";
+            this.b2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.dtp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+    }
+}
+
